18699daf1ec9f74c2019d56ede5ba98437f638a6
Dzaky Rifai <dzaky.pr.mulyono97@gmail.com>
2021-07-18 09:34:30 +0700

    learn soon
    Signed-off-by: Dzaky Rifai <dzaky.pr.mulyono97@gmail.com>

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,27 +1,57 @@
 import React from 'react';
 import { Container, Typography, Button, Grid } from '@material-ui/core';
-import { classes } from 'istanbul-lib-coverage';
+import { Link } from 'react-router-dom';
 
-const Cart = () => {
-  const isEmpty = true;
+import CartItem from './CartItem/CartItem';
+import useStyles from './styles';
 
-  const EmptyCart = () => {
-    <Typography variant="subtitle1">You have no items in your shopping cart, start adding some!</Typography>;
-  };
+const Cart = ({ cart, onUpdateCartQty, onRemoveFromCart, onEmptyCart }) => {
+  const classes = useStyles();
 
-  const FilledCart = () => {
+  const handleEmptyCart = () => onEmptyCart();
+
+  const renderEmptyCart = () => (
+    <Typography variant="subtitle1">
+      You have no items in your shopping cart,
+      <Link className={classes.link} to="/">
+        start adding some
+      </Link>
+      !
+    </Typography>
+  );
+
+  if (!cart.line_items) return 'Loading';
+
+  const renderCart = () => (
     <>
-      <Grid container spacing={3}></Grid>
-    </>;
-  };
+      <Grid container spacing={3}>
+        {cart.line_items.map((lineItem) => (
+          <Grid item xs={12} sm={4} key={lineItem.id}>
+            <CartItem item={lineItem} onUpdateCartQty={onUpdateCartQty} onRemoveFromCart={onRemoveFromCart} />
+          </Grid>
+        ))}
+      </Grid>
+      <div className={classes.cardDetails}>
+        <Typography variant="h4">Subtotal: {cart.subtotal.formatted_with_symbol}</Typography>
+        <div>
+          <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onClick={handleEmptyCart}>
+            Empty cart
+          </Button>
+          <Button className={classes.checkoutButton} component={Link} to="/checkout" size="large" type="button" variant="contained" color="primary">
+            Checkout
+          </Button>
+        </div>
+      </div>
+    </>
+  );
 
   return (
     <Container>
       <div className={classes.toolbar} />
-      <Typography className={classes.title} variant="h3">
+      <Typography className={classes.title} variant="h3" gutterBottom>
         Your Shopping Cart
       </Typography>
-      {isEmpty ? <EmptyCart /> : <FilledCart />}
+      {!cart.line_items.length ? renderEmptyCart() : renderCart()}
     </Container>
   );
 };
--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -3,25 +3,28 @@ import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } fro
 import { AddShoppingCart } from '@material-ui/icons';
 
 import useStyles from './styles';
-// import { Products } from '@chec/commerce.js/features/products';
 
 const Product = ({ product, onAddToCart }) => {
   const classes = useStyles();
+
+  const handleAddToCart = () => onAddToCart(product.id, 1);
 
   return (
     <Card className={classes.root}>
       <CardMedia className={classes.media} image={product.media.source} title={product.name} />
       <CardContent>
         <div className={classes.cardContent}>
-          <Typography variant="h5" gutterButtom>
+          <Typography gutterBottom variant="h5" component="h2">
             {product.name}
           </Typography>
-          <Typography variant="h5">{product.price.formatted_with_symbol}</Typography>
+          <Typography gutterBottom variant="h5" component="h2">
+            ${product.price.formatted}
+          </Typography>
         </div>
-        <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary" />
+        <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary" component="p" />
       </CardContent>
       <CardActions disableSpacing className={classes.cardActions}>
-        <IconButton aria-label="Add to Cart" onClick={() => onAddToCart(product.id, 1)}>
+        <IconButton aria-label="Add to Cart" onClick={handleAddToCart}>
           <AddShoppingCart />
         </IconButton>
       </CardActions>
